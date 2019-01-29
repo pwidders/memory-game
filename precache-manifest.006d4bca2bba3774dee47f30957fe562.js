38a6e244f52e6690ab3b9f89b1357407cd664b4c
Pete Widders <pwidders@hotmail.com>
2019-01-29 10:24:25 -0800

    Updates

---
+++ precache-manifest.006d4bca2bba3774dee47f30957fe562.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66c34b29d278a5d14e76",
+    "url": "/memory-game/static/js/runtime~main.66c34b29.js"
+  },
+  {
+    "revision": "f9d5cf3473ef81960c29",
+    "url": "/memory-game/static/js/main.f9d5cf34.chunk.js"
+  },
+  {
+    "revision": "c11296848bbc7da73a05",
+    "url": "/memory-game/static/js/1.c1129684.chunk.js"
+  },
+  {
+    "revision": "f9d5cf3473ef81960c29",
+    "url": "/memory-game/static/css/main.0c9cae63.chunk.css"
+  },
+  {
+    "revision": "10c91f1e802c7530204f38ed90e54569",
+    "url": "/memory-game/index.html"
+  }
+];
